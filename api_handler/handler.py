61554e5b1a331eb4bb4f6a198d5963c27441643f
pawaranand <pawaranand00@gmail.com>
2015-08-25 00:48:15 +0530

    [Session management]

--- api_handler/handler.py
+++ api_handler/handler.py
@@ -6,6 +6,7 @@ from frappe import _
 import frappe.sessions
 from response import build_response,report_error
 import api_handler
+import json	
 
 
 def handle():
@@ -16,8 +17,9 @@ def handle():
 	if op == 'login':
 		login_user()
 	elif cmd != 'login':
-		manage_user()
-		execute_cmd(cmd)	
+		user = manage_user()
+		if user:
+			execute_cmd(cmd)	
 
 	return build_response("json")
 
@@ -31,6 +33,7 @@ def execute_cmd(cmd, async=False):
 		if frappe.session['user'] == 'Guest':
 			if (method not in frappe.guest_methods):
 				return report_error(403,"Not Allowed")		
+
 		else:
 			if not method in frappe.whitelisted:
 				return report_error(403,"Not Allowed")
@@ -74,8 +77,32 @@ def login_user():
 	return ret
 
 def manage_user():
-	#pass
-	print frappe.form_dict.data
-	#add condition of user id 
-	#check given user id correct or not if not return from here 
-	#if user is valid set it to session resume
+	data = json.loads(frappe.form_dict.data)
+	sid = data.get('sid')
+	user_id = data.get('user_id')
+
+	if not sid:
+		report_error(417,"sid not provided")
+		return False		
+
+	elif sid and not user_id:
+		report_error(417,"user_id not provided")
+		return False
+
+	elif sid and user_id:
+		#user = frappe.db.get_value("User",{"user_id":user_id},"name")
+		user = "aaa"
+		if not user:
+			report_error(417,"user_id not provided")
+			return False
+		else:
+			try:
+				frappe.form_dict["sid"] = sid 
+				loginmgr = frappe.auth.LoginManager()
+			except frappe.SessionStopped,e:
+				http_status_code = getattr(e, "http_status_code", 500)
+				frappe.response["code"] = http_status_code
+				return False
+		
+	return True		
+
